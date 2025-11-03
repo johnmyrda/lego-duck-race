f399703a3984378f5b6822630184f3a3831ed7d5
John Myrda <hello@johnmyrda.com>
2025-11-03 12:55:11 -0600

    Add measurement logger test

---
+++ tests/utils/measurement_logger_test.py
@@ -0,0 +1,23 @@
+import time
+
+from lego_duck_race.utils.measurement_logger import MeasurementLogger
+
+
+def test_measurement_logger():
+    logger = MeasurementLogger(1000, "Test")
+    logger.info("initial log")
+    assert logger.last_log_time > 0
+    first_log_time = logger.last_log_time
+    time.sleep(0.1)
+    logger.info("second log")
+    assert logger.last_log_time == first_log_time
+
+
+def test_measurement_logger_multiple():
+    logger = MeasurementLogger(50, "Test")
+    logger.info("initial log")
+    assert logger.last_log_time > 0
+    first_log_time = logger.last_log_time
+    time.sleep(0.1)
+    logger.info("second log")
+    assert logger.last_log_time != first_log_time
